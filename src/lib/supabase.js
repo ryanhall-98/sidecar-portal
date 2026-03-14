e6530652842337287e23c8cede731bc1c355a871
Ryan Hall <ryan@Ryan.lan>
2026-03-14 00:09:00 -0400

    fix: hardcode supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://duyvxdctgutfjtqsampu.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1eXZ4ZGN0Z3V0Zmp0cXNhbXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMjE3NDcsImV4cCI6MjA4Nzg5Nzc0N30.DntNKVHR3EvQrJrLKlReFKOj2Sde4lXGsJ49LG1yCAc'
+);
